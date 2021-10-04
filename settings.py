d556fb61fba20422cd73f5c38f9f2692bf958001
blubber00 <blubber00@vk.com>
2021-10-04 11:46:08 +0300

    settings file
    Signed-off-by: blubber00 <blubber00@vk.com>

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-api_token = '2020468111:AAFNibhUsnA0PnirHDO_332pAaWMvkIKs_o'   #Токен от @BotFather в Telegram
+api_token = ''   #Токен от @BotFather в Telegram
 host_mysql = 'localhost'
 user_mysql = 'root'
 password_mysql = '123123'
 db_mysql = 'tbot_db'
 
-host_mysql1 = 'remotemysql.com'
-user_mysql1 = 'u7rwHVymRm'
-password_mysql1 = 'Rxe2BSJvTe'
-db_mysql1 = 'u7rwHVymRm'
+host_mysql1 = ''
+user_mysql1 = ''
+password_mysql1 = ''
+db_mysql1 = ''
