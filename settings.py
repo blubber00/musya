6536fc09f47275751a8512dc10a62186a238788d
blubber00 <91857028+blubber00@users.noreply.github.com>
2021-10-04 11:49:13 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+api_token = ''   #Токен от @BotFather в Telegram
+host_mysql = 'localhost'
+user_mysql = 'root'
+password_mysql = '123123'
+db_mysql = 'tbot_db'
+
+host_mysql1 = ''
+user_mysql1 = ''
+password_mysql1 = ''
+db_mysql1 = ''
